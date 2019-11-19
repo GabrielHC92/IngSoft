bea265fad63ced89e8de46302fba411914fed06e
GABRIELHC92 <pein1606@gmail.com>
2019-11-19 18:09:29 -0300

    pruebagit

--- test/TestFinanciera1.java
+++ test/TestFinanciera1.java
@@ -4,12 +4,18 @@
  * and open the template in the editor.
  */
 
+import laFinanciera.dominio.Credito;
+import laFinanciera.dominio.Plan;
+import laFinanciera.presentacion.ISolicitarCreditoVista;
+import laFinanciera.presentacion.SolicitarCreditoPresentador;
+import laFinanciera.vistas.SolicitarCreditoVista;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import ws.IServicioPublicoCreditoInformarCreditoOtorgadoErrorServicioFaultFaultMessage;
 
 /**
  *
@@ -22,7 +28,9 @@ public class TestFinanciera1 {
     
     @BeforeClass
     public static void setUpClass() {
+        
         //Setea el plan para usarlo en varias pruebas
+        
     }
     
     @AfterClass
@@ -43,9 +51,42 @@ public class TestFinanciera1 {
     // @Test
     // public void hello() {}
     @Test
+    public void calcularParametrosDelCreditoConPlanCuotaAdelantadaConTresCuotasCincoPorCientoInteresMontoDe10000() throws IServicioPublicoCreditoInformarCreditoOtorgadoErrorServicioFaultFaultMessage {
+        //Configuracion
+        float montoSolicitado=10000;
+        ISolicitarCreditoVista vista = new SolicitarCreditoVista();
+        SolicitarCreditoPresentador presentador = new SolicitarCreditoPresentador(vista);
+        Plan plan = new Plan("Cuota adelantada", (float) 0.05, 3);
+        //Ejecucion
+        //presentador.agregarDatosAlCredito(montoSolicitado, plan);
+        float creditoMontoSolicitado=presentador.getCredito().getMontoSolicitado();
+        float 
+        //Validacion
+        assertEquals(10000.0,creditoMontoSolicitado,0.01); 
+        assertEquals(11500.0, presentador.getCredito().calcularMontoTotal(),0.01);
+        assertEquals(3833.3333, presentador.getCredito().calcularCuotaInt(),0.0001);
+        assertEquals(6166.667, presentador.getCredito().importeAEntregar(),0.001);
+    }
+    
+    public void calcularParametrosDelCreditoConPlanCuotaAdelantadaConDoceCuotasSeisPorCientoInteresMontoDe10000() throws IServicioPublicoCreditoInformarCreditoOtorgadoErrorServicioFaultFaultMessage {
+        //Configuracion
+        float montoSolicitado=10000;
+        ISolicitarCreditoVista vista = new SolicitarCreditoVista();
+        SolicitarCreditoPresentador presentador = new SolicitarCreditoPresentador(vista);
+        Plan plan = new Plan("Cuota adelantada", (float) 0.06, 12);
+        
+        assertEquals(10000.0, presentador.getCredito().getMontoSolicitado(),0.01); 
+        assertEquals(17200.0, presentador.getCredito().calcularMontoTotal(),0.01);
+        assertEquals(1433.3333, presentador.getCredito().calcularCuotaInt(),0.0001);
+        assertEquals(8566.667, presentador.getCredito().importeAEntregar(),0.001);
+    }
+    /*
+    @Test
     public void ObtenerMontoaEntregarParaPlanCuotaAdelantadaconClienteJubilado(){
         //Secciones
         //Configuración: Declaración de objetos y datos
+        
+        
         //Crea un cliente y un crédito
         //Ejecución: Llamada de los métodos
         //Ejecuta el Crédito con Plan y Monto y obtengo el total a entregar
@@ -54,5 +95,5 @@ public class TestFinanciera1 {
                 //usar assert.equal y le paso el monto y un delta para el tema del redondeo por ej. 
                 //una dif de 0.1 donde se toma como válido
     }
-    
+    */
 }
